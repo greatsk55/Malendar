c4882f21c848a25a54694a5658da5924fe113d21
greatsk55 <greatsk553@naver.com>
2016-02-11 17:18:36 +0900

    month

--- app/src/main/java/com/example/user/malendar/ImageRecyclerAdapter.java
+++ app/src/main/java/com/example/user/malendar/ImageRecyclerAdapter.java
@@ -1,6 +1,7 @@
 package com.example.user.malendar;
 
 import android.content.Context;
+import android.media.ExifInterface;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -11,7 +12,10 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import org.w3c.dom.Text;
+
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -45,6 +49,14 @@ public class ImageRecyclerAdapter extends RecyclerView.Adapter<ImageRecyclerAdap
     public void onBindViewHolder(ViewHolder holder, int position) {
         //Picasso.with(mContext).load(sAlbumArtUri).placeholder(R.drawable.ic_no_album_sm).error(R.drawable.ic_no_album_sm).into(holder.albumArt);
         Picasso.with(mContext).load(new File(list.get(position).path)).into(holder.album);
+
+        try {
+            ExifInterface exifInterface = new ExifInterface(list.get(position).path);
+            String tmp = exifInterface.getAttribute(ExifInterface.TAG_DATETIME);
+            holder.text.setText(tmp.substring(0,4));
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -65,10 +77,12 @@ public class ImageRecyclerAdapter extends RecyclerView.Adapter<ImageRecyclerAdap
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public ImageView album;
+        public TextView text;
 
         public ViewHolder(View itemView) {
             super(itemView);
             album = (ImageView) itemView.findViewById(R.id.album_art1);
+            text = (TextView) itemView.findViewById(R.id.year);
 
             itemView.setClickable(true);
             itemView.setOnClickListener(this);
--- app/src/main/java/com/example/user/malendar/ViewImageActivity.java
+++ app/src/main/java/com/example/user/malendar/ViewImageActivity.java
@@ -2,6 +2,7 @@ package com.example.user.malendar;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.media.ExifInterface;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,11 +11,15 @@ import android.util.Log;
 
 import com.yqritc.recyclerviewflexibledivider.HorizontalDividerItemDecoration;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 /**
  * Created by user on 16. 2. 11.
  */
+
+//TODO 이미지에 해당 년도도 나오게하기, 액티비티 타이틀바에 해당 월일 나오게하기
 public class ViewImageActivity extends Activity {
 
     public RecyclerView recyclerView;
@@ -25,18 +30,26 @@ public class ViewImageActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        recyclerView = (RecyclerView) findViewById(R.id.list);
+        ArrayList<ImageList> list = (ArrayList<ImageList>) getIntent().getSerializableExtra("list");
 
+        recyclerView = (RecyclerView) findViewById(R.id.list);
         recyclerView.addItemDecoration(new HorizontalDividerItemDecoration.Builder(this).
                 color(Color.LTGRAY).sizeResId(R.dimen.divider).marginResId(R.dimen.leftmargin, R.dimen.rightmargin).build());
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
         recyclerView.setItemAnimator(new DefaultItemAnimator());
-
-        ArrayList<ImageList> list = (ArrayList<ImageList>) getIntent().getSerializableExtra("list");
 
         contactAdapter = new ImageRecyclerAdapter(this,
                 list, (LinearLayoutManager) recyclerView.getLayoutManager());
         recyclerView.setAdapter(contactAdapter);
+
+        try {
+            ExifInterface exifInterface = new ExifInterface(list.get(0).path);
+            String tmp = exifInterface.getAttribute(ExifInterface.TAG_DATETIME);
+
+            setTitle(tmp.substring(5,7)+"월 "+ tmp.substring(8,10)+"일");
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
