745e4179425ec8ecc61dc08d941456095fdc2140
sk <greatsk553@naver.com>
2016-11-14 17:16:35 +0900

    퍼미션 수정

--- app/src/main/java/com/example/user/malendar/MainActivity.java
+++ app/src/main/java/com/example/user/malendar/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.malendar;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.ContentResolver;
@@ -20,6 +21,8 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
 import com.squareup.timessquare.CalendarPickerView;
 
 public class MainActivity extends Activity {
@@ -40,47 +43,72 @@ public class MainActivity extends Activity {
 
         calendar.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
             @Override
-            public void onDateSelected(Date date) {
+            public void onDateSelected(final Date date) {
 
-                ArrayList<ImageList> result = new ArrayList<ImageList>();
-                Uri uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                String[] projection = { MediaColumns.DATA, MediaColumns.DISPLAY_NAME };
+                PermissionListener permissionlistener = new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted() {
+                        ArrayList<ImageList> result = new ArrayList<ImageList>();
+                        Uri uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                        String[] projection = { MediaColumns.DATA, MediaColumns.DISPLAY_NAME };
 
-                Cursor cursor = getContentResolver().query(uri, projection, null, null, MediaColumns.DATE_ADDED + " desc");
-                int columnIndex = cursor.getColumnIndexOrThrow(MediaColumns.DATA);
-                int columnDisplayname = cursor.getColumnIndexOrThrow(MediaColumns.DISPLAY_NAME);
+                        Cursor cursor = getContentResolver().query(uri, projection, null, null, MediaColumns.DATE_ADDED + " desc");
+                        int columnIndex = cursor.getColumnIndexOrThrow(MediaColumns.DATA);
+                        int columnDisplayname = cursor.getColumnIndexOrThrow(MediaColumns.DISPLAY_NAME);
 
-                int lastIndex;
-                while (cursor.moveToNext())
-                {
-                    String absolutePathOfImage = cursor.getString(columnIndex);
-                    String nameOfFile = cursor.getString(columnDisplayname);
-                    lastIndex = absolutePathOfImage.lastIndexOf(nameOfFile);
-                    lastIndex = lastIndex >= 0 ? lastIndex : nameOfFile.length() - 1;
+                        int lastIndex;
+                        while (cursor.moveToNext())
+                        {
+                            String absolutePathOfImage = cursor.getString(columnIndex);
+                            String nameOfFile = cursor.getString(columnDisplayname);
+                            lastIndex = absolutePathOfImage.lastIndexOf(nameOfFile);
+                            lastIndex = lastIndex >= 0 ? lastIndex : nameOfFile.length() - 1;
 
-                    if (!TextUtils.isEmpty(absolutePathOfImage))
-                    {
-                        try {
-                            ExifInterface exifInterface = new ExifInterface(absolutePathOfImage);
-                            String tmp = exifInterface.getAttribute(ExifInterface.TAG_DATETIME);
-                            if(tmp==null) continue;
-                            Calendar cal = Calendar.getInstance();
-                            cal.setTime(date);
-                            cal.set(Calendar.MONTH, cal.get(Calendar.MONTH)+1);
+                            if (!TextUtils.isEmpty(absolutePathOfImage))
+                            {
+                                try {
+                                    ExifInterface exifInterface = new ExifInterface(absolutePathOfImage);
+                                    String tmp = exifInterface.getAttribute(ExifInterface.TAG_DATETIME);
+                                    if(tmp==null) continue;
+                                    Calendar cal = Calendar.getInstance();
+                                    cal.setTime(date);
+                                    cal.set(Calendar.MONTH, cal.get(Calendar.MONTH)+1);
 
-                            if(cal.get(Calendar.MONTH)==Integer.parseInt(tmp.substring(5, 7)) &&
-                                    cal.get(Calendar.DATE)==Integer.parseInt(tmp.substring(8, 10))){
-                                Log.i("dat2", absolutePathOfImage);
-                                result.add(new ImageList(absolutePathOfImage,nameOfFile));
+                                    if(cal.get(Calendar.MONTH)==Integer.parseInt(tmp.substring(5, 7)) &&
+                                            cal.get(Calendar.DATE)==Integer.parseInt(tmp.substring(8, 10))){
+                                        Log.i("dat2", absolutePathOfImage);
+                                        result.add(new ImageList(absolutePathOfImage,nameOfFile));
+                                    }
+
+                                }catch(IOException e){
+                                    e.printStackTrace();
+                                }
+                                //result.add(absolutePathOfImage);
                             }
-
-                        }catch(IOException e){
-                            e.printStackTrace();
                         }
-                        //result.add(absolutePathOfImage);
+                        if( result.size()>0) openActivity(result);
                     }
-                }
-                if( result.size()>0) openActivity(result);
+
+                    @Override
+                    public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                        Toast.makeText(MainActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+                    }
+
+
+                };
+
+
+                new TedPermission(getApplicationContext())
+                        .setPermissionListener(permissionlistener)
+                        .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+                        .setPermissions(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        .check();
+
+
+
+
+
+
             }
 
             @Override
