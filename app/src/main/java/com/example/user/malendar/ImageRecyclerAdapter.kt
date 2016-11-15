4cbb961b51017cb2892195da3a4924aadfc5d5ac
sk <greatsk553@naver.com>
2016-11-15 09:47:35 +0900

    코틀린으로 리팩토링

--- app/src/main/java/com/example/user/malendar/ImageRecyclerAdapter.kt
+++ app/src/main/java/com/example/user/malendar/ImageRecyclerAdapter.kt
@@ -1,24 +1,18 @@
 package com.example.user.malendar;
 
-import android.content.Context;
-import android.media.ExifInterface;
-import android.net.Uri;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
-import com.squareup.picasso.Picasso;
-
-import org.w3c.dom.Text;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
+import android.content.Context
+import android.media.ExifInterface
+import android.net.Uri
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import java.io.IOException
+import java.util.*
 
 /**
  * Created by user on 16. 2. 11.
@@ -37,13 +31,12 @@ class ImageRecyclerAdapter(private val mContext: Context, var contactsList: Arra
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        //Picasso.with(mContext).load(sAlbumArtUri).placeholder(R.drawable.ic_no_album_sm).error(R.drawable.ic_no_album_sm).into(holder.albumArt);
         Glide.with(mContext).load(Uri.fromFile(contactsList!![position].path)).thumbnail(0.1f).into(holder.album)
 
         try {
             val exifInterface = ExifInterface(contactsList!![position].path.absolutePath)
             val tmp = exifInterface.getAttribute(ExifInterface.TAG_DATETIME)
-            holder.text.text = tmp.substring(0, 4)
+            holder.text.setText(tmp.substring(0, 4));
         } catch (e: IOException) {
             e.printStackTrace()
         }
@@ -55,23 +48,15 @@ class ImageRecyclerAdapter(private val mContext: Context, var contactsList: Arra
         return contactsList!!.size
     }
 
-    fun remove(position: Int) {
-        contactsList!!.removeAt(position)
-        notifyItemRemoved(position)
-    }
-
-    fun add(song: ImageList, position: Int) {
-        contactsList!!.add(position, song)
-        notifyItemInserted(position)
-    }
-
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
-        var album: ImageView
-        var text: TextView
+        public val album by lazy {
+            itemView.findViewById(R.id.album_art1) as ImageView
+        }
+        public val text by lazy {
+            itemView.findViewById(R.id.year) as TextView
+        }
 
         init {
-            album = itemView.findViewById(R.id.album_art1) as ImageView
-            text = itemView.findViewById(R.id.year) as TextView
 
             itemView.isClickable = true
             itemView.setOnClickListener(this)
--- app/src/main/java/com/example/user/malendar/MainActivity.kt
+++ app/src/main/java/com/example/user/malendar/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.example.user.malendar;
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.media.ExifInterface
 import android.os.Bundle
 import android.provider.MediaStore.MediaColumns
-import android.support.v7.app.AppCompatActivity
 import android.text.TextUtils
 import android.util.Log
 import android.widget.Toast
 import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import com.squareup.timessquare.CalendarPickerView
 import java.io.File
 import java.io.IOException
@@ -35,64 +36,67 @@ public class MainActivity : Activity() {
         calendar.init( today, lastMonth.getTime())
                 .withSelectedDate(today);
 
-
-
         calendar.setOnDateSelectedListener( object :  CalendarPickerView.OnDateSelectedListener {
             override fun onDateSelected(date : Date) {
-                object : PermissionListener {
-                    override fun onPermissionGranted() {
-                        val result = ArrayList<ImageList>()
-                        val uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-                        val projection = arrayOf(MediaColumns.DATA, MediaColumns.DISPLAY_NAME)
 
-                        val cursor = contentResolver.query(uri, projection, null, null, MediaColumns.DATE_ADDED + " desc")
-                        val columnIndex = cursor!!.getColumnIndexOrThrow(MediaColumns.DATA)
-                        val columnDisplayname = cursor.getColumnIndexOrThrow(MediaColumns.DISPLAY_NAME)
+                TedPermission(applicationContext)
+                        .setPermissionListener(object : PermissionListener {
+                            override fun onPermissionGranted() {
+                                val result = ArrayList<ImageList>()
+                                val uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                                val projection = arrayOf(MediaColumns.DATA, MediaColumns.DISPLAY_NAME)
 
-                        var month : String = "0"
-                        var day : String  = "0"
+                                val cursor = contentResolver.query(uri, projection, null, null, MediaColumns.DATE_ADDED + " desc")
+                                val columnIndex = cursor!!.getColumnIndexOrThrow(MediaColumns.DATA)
+                                val columnDisplayname = cursor.getColumnIndexOrThrow(MediaColumns.DISPLAY_NAME)
 
-                        var lastIndex: Int
+                                var month : String = "0"
+                                var day : String  = "0"
 
-                        while (cursor.moveToNext()) {
-                            val absolutePathOfImage = cursor.getString(columnIndex)
-                            val nameOfFile = cursor.getString(columnDisplayname)
-                            lastIndex = absolutePathOfImage.lastIndexOf(nameOfFile)
-                            lastIndex = if (lastIndex >= 0) lastIndex else nameOfFile.length - 1
+                                var lastIndex: Int
 
-                            if (!TextUtils.isEmpty(absolutePathOfImage)) {
-                                try {
-                                    val exifInterface = ExifInterface(absolutePathOfImage)
-                                    val tmp = exifInterface.getAttribute(ExifInterface.TAG_DATETIME) ?: continue
-                                    val cal = Calendar.getInstance()
-                                    cal.time = date
-                                    cal.set(Calendar.MONTH, cal.get(Calendar.MONTH) + 1)
+                                while (cursor.moveToNext()) {
+                                    val absolutePathOfImage = cursor.getString(columnIndex)
+                                    val nameOfFile = cursor.getString(columnDisplayname)
+                                    lastIndex = absolutePathOfImage.lastIndexOf(nameOfFile)
+                                    lastIndex = if (lastIndex >= 0) lastIndex else nameOfFile.length - 1
 
-                                    month = tmp.substring(5, 7)
-                                    day = tmp.substring(8, 10)
+                                    if (!TextUtils.isEmpty(absolutePathOfImage)) {
+                                        try {
+                                            val exifInterface = ExifInterface(absolutePathOfImage)
+                                            val tmp = exifInterface.getAttribute(ExifInterface.TAG_DATETIME) ?: continue
+                                            val cal = Calendar.getInstance()
+                                            cal.time = date
+                                            cal.set(Calendar.MONTH, cal.get(Calendar.MONTH) + 1)
 
-                                    if (cal.get(Calendar.MONTH) === Integer.parseInt(tmp.substring(5, 7))
-                                            && cal.get(Calendar.DATE) === Integer.parseInt(tmp.substring(8, 10))) {
-                                        Log.i("dat2", absolutePathOfImage)
-                                        result.add(ImageList(File(absolutePathOfImage), nameOfFile))
+                                            month = tmp.substring(5, 7)
+                                            day = tmp.substring(8, 10)
+
+                                            if (cal.get(Calendar.MONTH) === Integer.parseInt(tmp.substring(5, 7))
+                                                    && cal.get(Calendar.DATE) === Integer.parseInt(tmp.substring(8, 10))) {
+                                                Log.i("dat2", absolutePathOfImage)
+                                                result.add(ImageList(File(absolutePathOfImage), nameOfFile))
+                                            }
+
+                                        } catch (e: IOException) {
+                                            e.printStackTrace()
+                                        }
+
+                                        //result.add(absolutePathOfImage);
                                     }
-
-                                } catch (e: IOException) {
-                                    e.printStackTrace()
                                 }
-
-                                //result.add(absolutePathOfImage);
+                                if (result.size > 0) openActivity(result, month, day)
                             }
-                        }
-                        if (result.size > 0) openActivity(result, month, day)
-                    }
 
-                    override fun onPermissionDenied(deniedPermissions: ArrayList<String>) {
-                        Toast.makeText(this@MainActivity, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show()
-                    }
+                            override fun onPermissionDenied(deniedPermissions: ArrayList<String>) {
+                                Toast.makeText(this@MainActivity, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show()
+                            }
 
 
-                }
+                        })
+                        .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+                        .setPermissions(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        .check();
 
             }
             override fun onDateUnselected(date : Date ) {
